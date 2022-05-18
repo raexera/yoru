f1a8630cba9f662655f773f5d007195bbaf96458
Yu Gu <guyu2876@gmail.com>
2022-05-18 21:22:26 +0800

    fix: error of dock

--- config/awesome/ui/dock/dock.lua
+++ config/awesome/ui/dock/dock.lua
@@ -48,7 +48,7 @@ local function helpers()
 		local B, K, OUT, I, D = 16, "0123456789ABCDEF", "", 0
 		while IN > 0 do
 			I = I + 1
-			IN, D = math.floor(IN / B), (IN % B) + 1
+			IN, D = math.floor(IN / B), math.floor((IN % B) + 1)
 			OUT = string.sub(K, D, D) .. OUT
 		end
 		return #OUT == 2 and OUT or "0" .. OUT
