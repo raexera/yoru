e35ced674e5639a3ff181a969997fbec0a3b860d
TheBearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2023-02-08 16:02:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
+#!/bin/env bash
+set -e
+
+echo "Welcome!" && sleep 2
+
+#Default vars
+HELPER="paru"
+
+# does full system update
+echo "Doing a system update, cause stuff may break if it's not the latest version..."
+sudo pacman --noconfirm -Syu
+
+echo "###########################################################################"
+echo "Will do stuff, get ready"
+echo "###########################################################################"
+
+# install base-devel if not installed
+sudo pacman -S --noconfirm --needed base-devel wget git
+
 # Choose an AUR helper and install needed dependencies + Awesome itself (the git version of course)
 echo "We need an AUR helper. It is essential. 1) paru       2) yay"
 read -r -p "What is the AUR helper of your choice? (Default is paru): " num
@@ -9,7 +28,7 @@ fi
 
 if ! command -v $HELPER &> /dev/null
 then
-    echo "It seems that you don't have $HELPER installed, I'll install that for you before>
+    echo "It seems that you don't have $HELPER installed, I'll install that for you before continuing."
         git clone https://aur.archlinux.org/$HELPER.git ~/.srcs/$HELPER
         (cd ~/.srcs/$HELPER/ && makepkg -si )
 fi
@@ -49,3 +68,7 @@ $HELPER -Sy --needed picom-git  \
 
 sudo systemctl --user enable mpd.service
 sudo systemctl --user start mpd.service
+
+# Clone the repo :D
+git clone --depth 1 --recurse-submodules https://github.com/rxyhn/yoru.git
+cd yoru && git submodule update --remote --merge
