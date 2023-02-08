5ac1b0af2ee8f762ea1a0cfaa5c555b2112428b3
TheBearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2023-02-08 15:46:40 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
+echo "We need an AUR helper. It is essential. 1) paru       2) yay"
+read -r -p "What is the AUR helper of your choice? (Default is paru): " num
 
+if [ $num -eq 2 ]
+then
+    HELPER="yay"
+fi
+
+if ! command -v $HELPER &> /dev/null
+then
+    echo "It seems that you don't have $HELPER installed, I'll install that for you before>
+        git clone https://aur.archlinux.org/$HELPER.git ~/.srcs/$HELPER
+        (cd ~/.srcs/$HELPER/ && makepkg -si )
+fi
+
+$HELPER -Sy
